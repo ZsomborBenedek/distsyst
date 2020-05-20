19febca9e432d852a3f9132b00e54dee1bfa01b8
Benedek Zsombor Ákos <zsomborbeni@gmail.com>
2020-05-20 16:02:07 +0200

    Update FileController.java

--- server/src/main/java/be/uantwerpen/server/controller/FileController.java
+++ server/src/main/java/be/uantwerpen/server/controller/FileController.java
@@ -1,7 +1,9 @@
 package be.uantwerpen.server.controller;
 
 import org.springframework.web.bind.annotation.RestController;
-import be.uantwerpen.server.service.NodeRepository;
+import be.uantwerpen.server.service.NodeLogic;
+// import be.uantwerpen.server.service.NodeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,15 +12,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 public class FileController {
 
-    NodeRepository nodeRepository;
+    @Autowired
+    NodeLogic logic;
 
-    public FileController() {
-        this.nodeRepository = new NodeRepository();
-    }
+    // public FileController(NodeLogic logic) {
+    //     this.logic = new NodeLogic(new NodeRepository());
+    // }
 
     @GetMapping(value = "getfile/{fileName}")
     public ResponseEntity<String> getFileOwnerIp(@PathVariable String fileName) {
-        String ownerIp = nodeRepository.getFileOwnerIp(fileName);
+        System.out.println("Searched file's name: " + fileName);
+        String ownerIp = this.logic.getFileOwnerIp(fileName);
+        System.out.println("Searched owner's ip: " + ownerIp + "\n");
         return new ResponseEntity<String>(ownerIp, HttpStatus.OK);
     }
 }
